e248a9d83042472471e528c76ff1b3e1b8f96fd1
PedroBrasil111 <p260637@dac.unicamp.br>
2023-09-04 10:15:15 -0300

    deletar arquivos

--- teste.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x = (~8) + 1;
-    
-    printf("%d %x\n", x, x);
-    // 00545648
-    // 48565400
-}
--- teste2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x = 0x9;
-    printf("%d\n", x);
-}
