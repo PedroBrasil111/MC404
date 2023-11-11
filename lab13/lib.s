160f6925c0761646dea3a0c99d2f350dbb3538da
PedroBrasil111 <p260637@dac.unicamp.br>
2023-11-11 16:54:02 -0300

    lab13 funcionando

--- lab13/lib.s
+++ lab13/lib.s
@@ -3,24 +3,17 @@
 isr_stack: # Final da pilha das ISRs
 .skip 1024 # Aloca 1024 bytes para a pilha
 isr_stack_end: # Base da pilha das ISRs
-.align 2
 _system_time: .skip 4
 
-.data
-.align 2
-buffer: .asciz "aaa"
-
 .section .text
-.align 2
 .globl _start
 .globl _system_time
 .globl play_note
-.globl main_isr
 
 # 0xFFFF0100 - 0xFFFF0300 - general_purpose_timer.js
 .set GPT_READ, 0xFFFF0100  # byte  - Storing “1” triggers the GPT device to start reading the current system time. The register is set to 0 when the reading is completed
-.set GPT_TIME, 0xFFFF0100  # word  - Stores the time (in milliseconds) at the moment of the last reading by the GPT
-.set GPT_INT, 0xFFFF0100   # word  - Storing v > 0 programs the GPT to generate an external interruption after v milliseconds. It also sets this register to 0 after v milliseconds (immediately before generating the interruption)
+.set GPT_TIME, 0xFFFF0104  # word  - Stores the time (in milliseconds) at the moment of the last reading by the GPT
+.set GPT_INT, 0xFFFF0108   # word  - Storing v > 0 programs the GPT to generate an external interruption after v milliseconds. It also sets this register to 0 after v milliseconds (immediately before generating the interruption)
 # 0xFFFF0300 - 0xFFFF0500 - midi_synthesizer.js
 .set MIDI_CH, 0xFFFF0300   # byte  - Storing ch ≥ 0 triggers the synthesizer to start playing a MIDI note in the channel ch
 .set MIDI_INST, 0xFFFF0302 # short - Instrument ID
@@ -61,15 +54,14 @@ _start:
     # initialize GPT
     li t0, 100
     li t1, GPT_INT
-    sw t0, (t1) # set interruption interval as 100 ms
+    sw t0, (t1) # sets interruption interval to 100 ms
+    #jal main_isr
     jal main # plays music
-    li a0, 0
-    jal exit # exit with code 0
-.align 2
+
 main_isr:
     # Salvar o contexto
     csrrw sp, mscratch, sp  # Troca sp com mscratch
-    addi sp, sp, -12        # Aloca espaço na pilha da ISRs
+    addi sp, sp, -16        # Aloca espaço na pilha da ISRs
     sw t0, 0(sp)
     sw t1, 4(sp)
     sw t2, 8(sp)
@@ -78,6 +70,10 @@ main_isr:
     lw t2, (t1)
     addi t2, t2, 100
     sw t2, (t1)
+    csrr t0, mepc # load return address (address of
+    # the instruction that invoked the syscall)
+    addi t0, t0, 4 # adds 4 to the return address (to return after ecall)
+    csrw mepc, t0 # stores the return address back on mepc
     li t0, 100
     li t1, GPT_INT
     sw t0, (t1) # set interruption interval as 100 ms
@@ -85,14 +81,6 @@ main_isr:
     lw t0, 0(sp)
     lw t1, 4(sp)
     lw t2, 8(sp)
-    addi sp, sp, 12         # Desaloca espaço da pilha da ISR
+    addi sp, sp, 16         # Desaloca espaço da pilha da ISR
     csrrw sp, mscratch, sp  # Troca sp com mscratch novamente
     mret # Retorna da interrupção
-
-# Terminate calling process
-# Parameters: a0 - status code
-# No return value
-exit:
-    li a0, 0
-    li a7, 93    # syscall exit (93)
-    ecall
